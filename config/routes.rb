64cb13bbab4c18cefb96d5cef8370aedcab6e533
смагина марина <smaginamarina@MacBook-Air-smagina.local>
2017-04-01 11:46:30 +0600

    Added model Gallery and root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :galleries
   devise_for :users
+  root "galleries#index"
   # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401052922) do
+ActiveRecord::Schema.define(version: 20170401054204) do
+
+  create_table "galleries", force: :cascade do |t|
+    t.string   "title"
+    t.string   "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
